c878b89999c93f58ccfaacd4afd9a7fdcd950480
edward.gao <edward.gao@logicmonitor.com>
2018-08-07 18:04:25 +0800

    a compile error

--- java/main/com/logicmonitor/xensimulator/utils/SimAPI.java
+++ java/main/com/logicmonitor/xensimulator/utils/SimAPI.java
@@ -1,5 +1,7 @@
 package com.logicmonitor.xensimulator.utils;
 
+import java.lang.annotation.*;
+
 /**
  * Indicates this is a remote api which will be used to change the
  *  simulator internal status or variables
